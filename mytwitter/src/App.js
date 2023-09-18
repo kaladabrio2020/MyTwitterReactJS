205498757fd7d41051e13ccab61205d815478fdb
Mateus Sousa do Carmo <mateus.1090ptbr0@gmail.com>
2023-09-17 21:29:26 -0300

    Inicio do projeto & learning

---
+++ mytwitter/src/App.js
@@ -0,0 +1,8 @@
+import MainPage from "./components/MainPage/MainPage";
+
+export default function App() {
+    return (
+      <MainPage />
+    );
+}
+
---
+++ mytwitter/src/components/MainPage/MainPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Styles from "./MainPage.module.css";
+
+export default function MainPage(){
+    return(
+        <div className={Styles.DivMain}>
+            <p> HELLO WORLD </p>
+        </div>
+    )
+}
