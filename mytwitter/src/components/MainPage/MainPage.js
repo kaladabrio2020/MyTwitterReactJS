0cf06b2f26c4f4ff9a2bbdaee9a6fdcef3ce203b
Mateus Sousa do Carmo <mateus.1090ptbr0@gmail.com>
2023-09-18 10:45:21 -0300

    TelaDeLogin

--- mytwitter/src/components/MainPage/MainPage.js
+++ mytwitter/src/components/MainPage/MainPage.js
@@ -3,8 +3,22 @@ import Styles from "./MainPage.module.css";
 
 export default function MainPage(){
     return(
-        <div className={Styles.DivMain}>
-            <p> HELLO WORLD </p>
-        </div>
+        <body>
+            <div className={Styles.DivMain}>
+
+                <div className={Styles.DivText}>
+                    <h1>My Twitter</h1>
+                    <p>Um projeto de cada de Tecnicas de Programação I feito em java agora em React js.</p>
+                </div>  
+
+                <div className={Styles.DivUser}>
+                    <input placeholder="E-mail ou Cpf" />   <br></br>
+                    <input placeholder="Senha" />           <br></br>
+                    <button title="Entrar"/>
+                </div>      
+            
+            </div>
+        
+        </body>
     )
 }
